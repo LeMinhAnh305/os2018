94b6319f7835dc6d35a5b611bf41f9d96ff06b8c
Le Minh Anh <leeminhanh98@gmail.com>
2018-01-13 11:26:58 +0700

    Practical Work 4

--- 04.pratical.work.fork.exec.c
+++ 04.pratical.work.fork.exec.c
@@ -4,17 +4,17 @@ int main() {
     printf("Main before fork()\n ");
     int pid=fork();
     if (pid ==0) {
-    printf(" I am child after fork(), launching ps -ef\n");
-    char *args[]= { "bin/ps","-ef", NULL};
-    execvp("/bin/ps",args);
-    printf("Finished launching ps -ef\n");
-    }
+    	printf(" I am child after fork(), launching ps -ef\n");
+   	char *args[]= { "bin/ps","-ef", NULL};
+    	execvp("/bin/ps",args);
+    	printf("Finished launching ps -ef\n");
+    	}
     int pid1=fork();
     if (pid1 ==0) {
-    printf(" I am child after fork(), launching free -h\n");
-    char *args1[]= { "free","-h", NULL};
-    execvp("free",args1);
-    printf("Finished launching free -h\n");
-    }
+    	printf(" I am child after fork(), launching free -h\n");
+    	char *args1[]= { "free","-h", NULL};
+    	execvp("free",args1);
+    	printf("Finished launching free -h\n");
+    	}
 return 0;
 }
